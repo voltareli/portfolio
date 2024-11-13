f9033821717c256b8f6af56e15a6d8b61cf30482
Nícolas Voltareli Coelho <ra54152069813@cursos.insf>
2024-11-13 17:41:35 -0300

    oi

---
+++ src/pages/App.jsx
@@ -0,0 +1,48 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="principal">
+      <div className='fundo'>
+        <div className='conteudo'>
+          <div className='infos'>
+            <h2>Olá, Sou o <br />Nicolas Voltareli</h2>
+            Nicolas Voltareli
+            <p>Seu desenvolver <strong>full-stack</strong></p>
+
+            <div className='curriculo-contato'>
+
+              <div className='botão1'>
+                <button className='curriculo'>Download CV</button>
+              </div>
+
+              <div className='botão2'>
+                <button className=''>Entra em contato</button>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className='foto'>
+
+
+
+          </div>
+
+
+        </div>
+
+        <div className='sobre'>
+          <p>Sou desenvolvedor Full Stack iniciante, com conhecimentos em JavaScript, Node.js, React.js e MySQL. Formado em Técnico em Desenvolvimento de Sistemas pelo Instituto Social Nossa Senhora de Fátima, estou em busca de oportunidades para aplicar minhas habilidades no desenvolvimento de aplicações completas, tanto no front-end quanto no back-end. Meu objetivo é continuar aprendendo e crescer profissionalmente, contribuindo para projetos desafiadores e de impacto.</p>
+        </div>
+
+        <div className='habilidades'>
+          <p>jv</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
