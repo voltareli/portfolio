0f7d3401c082e7ac87eecdc0f526c5b7ed3e5e2b
Nícolas Voltareli Coelho <ra54152069813@cursos.insf>
2024-11-13 18:28:06 -0300

    oi

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,1 +1,34 @@
 import './header.scss';
+
+
+export default function header() {
+
+    return (
+        <div className='mae'>
+
+            <div className='informacoes'>
+
+
+                <div className='primeira'>
+                    <p>oi</p>
+                </div>
+
+                <div className='segunda'>
+                    <p>Portifólio</p>
+                </div>
+
+                <div className='terceira'>
+
+                    <p>insta</p>
+                    <p>linkedin</p>
+
+                </div>
+
+
+
+            </div>
+
+           <div className='linha'></div>
+        </div>
+    )
+};
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,44 +1,52 @@
 import './App.scss';
-
+import Eu from '../assets/foto.png'
+import Header from '../components/header/header';
 function App() {
   return (
     <div className="principal">
       <div className='fundo'>
-        <div className='conteudo'>
-          <div className='infos'>
-            <h2>Olá, Sou o <br />Nicolas Voltareli</h2>
-            Nicolas Voltareli
-            <p>Seu desenvolver <strong>full-stack</strong></p>
+        <Header/>
 
-            <div className='curriculo-contato'>
 
-              <div className='botão1'>
-                <button className='curriculo'>Download CV</button>
-              </div>
+      
+        <div className='lang'>
 
-              <div className='botão2'>
-                <button className=''>Entra em contato</button>
-              </div>
+        <div className='infos'>
 
+          <h2>Olá, Sou o <br />Nicolas Voltareli</h2>
+          <p>Seu desenvolver <strong>full-stack</strong></p>
+
+          <div className='curriculo-contato'>
+
+            <div className='botao1'>
+              <button className='curriculo'>Download CV</button>
             </div>
 
-          </div>
-
-          <div className='foto'>
-
-
-
+            <div className='botao2'>
+              <button className=''>Entra em contato</button>
+            </div>
           </div>
 
 
         </div>
 
+        <div className='foto'>
+
+          <img src={Eu} alt="" />
+
+        </div>
+
+        </div>
+
+
         <div className='sobre'>
+
+          <h1>Sobre mim</h1>
           <p>Sou desenvolvedor Full Stack iniciante, com conhecimentos em JavaScript, Node.js, React.js e MySQL. Formado em Técnico em Desenvolvimento de Sistemas pelo Instituto Social Nossa Senhora de Fátima, estou em busca de oportunidades para aplicar minhas habilidades no desenvolvimento de aplicações completas, tanto no front-end quanto no back-end. Meu objetivo é continuar aprendendo e crescer profissionalmente, contribuindo para projetos desafiadores e de impacto.</p>
         </div>
 
         <div className='habilidades'>
-          <p>jv</p>
+
         </div>
       </div>
     </div>
