cb26a361733410269890365a5ef05a998c6cafc5
Nícolas Voltareli Coelho <ra54152069813@cursos.insf>
2024-11-19 14:19:00 -0300

    finalixado

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,126 +1,110 @@
 import './App.scss';
-import Eu from '../assets/foto.png'
+import Eu from '../assets/foto.png';
 import Header from '../components/header/header';
-import Card from '../components/card-habilidade/card'
-import Sql from '../assets/mysql.png'
-import Js from '../assets/java.png'
-import Html from '../assets/images-removebg-preview.png'
-import React1 from '../assets/react.png'
-import Node from '../assets/node.png'
-import Projetu from '../components/card-projeto/projeto'
-import Site from '../assets/site.png'
-
-
-
+import Card from '../components/card-habilidade/card';
+import Sql from '../assets/mysql.png';
+import Js from '../assets/java.png';
+import Html from '../assets/images-removebg-preview.png';
+import React1 from '../assets/react.png';
+import Node from '../assets/node.png';
+import Projetu from '../components/card-projeto/projeto';
+import Site from '../assets/site.png';
+import Cv from '../assets/cv.pdf';
 
 function App() {
-
   const cards = [
     { imagem: Js, nome: 'Java Script', descricao: 'Linguagem de programação para o front-end e back-end.' },
     { imagem: Node, nome: 'NodeJs', descricao: 'Linguagem de programação para o front-end e back-end.' },
     { imagem: Html, nome: 'Html', descricao: 'Linguagem de programação para o front-end e back-end.' },
     { imagem: React1, nome: 'ReactJs', descricao: 'Linguagem de programação para o front-end e back-end.' },
     { imagem: Sql, nome: 'MySQL', descricao: 'Linguagem de programação para o front-end e back-end.' }
-];
+  ];
+
+  const fazerDownload = () => {
+    const link = document.createElement('a');  
+    link.href = Cv;  
+    link.download = 'Cv_NicolasVoltareli.pdf';  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="principal">
-      <div className='fundo'>
-        
+      <div className="fundo">
         <Header />
 
+        <section id="home">
+          <div className="lang">
+            <div className="infos">
+              <h2 className="nom1">
+                Olá, Sou o <br /> <span className="nome">Nicolas Voltareli</span>
+              </h2>
+              <p>Seu desenvolvedor <strong>full-stack</strong></p>
 
-        <section id='home'>
+              <div className="curriculo-contato">
+                <div className="botao1">
+                  <button onClick={fazerDownload} className="curriculo">Download CV</button>
+                </div>
 
-        <div className='lang'>
-
-          <div className='infos'>
-
-            <h2 className='nom1'>Olá, Sou o <br /> <span className='nome'>Nicolas Voltareli</span></h2>
-            <p>Seu desenvolver <strong>full-stack</strong></p>
-
-            <div className='curriculo-contato'>
-
-              <div className='botao1'>
-                <a href='https://1drv.ms/b/s!ArgL7TJn49UT5g_GkrR2MeQkcPyR?e=byn6Gd' download className='botao-download'>
-
-                <button className='curriculo'>Download CV</button>
-                </a>
-              </div>
-
-              <div className='botao2'>
-              <a href="mailto:nivoltareli@gmail.com">
-
-                <button className=''>Entra em contato</button>
-                </a>
+                <div className="botao2">
+                  <a href="mailto:nivoltareli@gmail.com">
+                    <button className="">Entre em contato</button>
+                  </a>
+                </div>
               </div>
             </div>
 
-
+            <div className="foto">
+              <img src={Eu} alt="Foto de Nicolas Voltareli" />
+            </div>
           </div>
-
-          <div className='foto'>
-
-            <img src={Eu} alt="" />
-
-          </div>
-
-        </div>
         </section>
 
-      <section id='sobre'>
-
-        <div className='sobre'>
-
-          <h1>Sobre mim</h1>
-          <p>Sou desenvolvedor Full Stack iniciante, com conhecimentos em JavaScript, Node.js, React.js e MySQL. Formado em Técnico em Desenvolvimento de Sistemas pelo Instituto Social Nossa Senhora de Fátima, estou em busca de oportunidades para aplicar minhas habilidades no desenvolvimento de aplicações completas, tanto no front-end quanto no back-end. Meu objetivo é continuar aprendendo e crescer profissionalmente, contribuindo para projetos desafiadores e de impacto.</p>
-        </div>
-
-      </section>
-      <section id='habilidades'>
-
-        <div className='habilidades'>
-
-          <div className='titulo'>
-            <h1>Minhas habilidades</h1>
+        <section id="sobre">
+          <div className="sobre">
+            <h1>Sobre mim</h1>
+            <p>
+              Sou desenvolvedor Full Stack iniciante, com conhecimentos em JavaScript, Node.js, React.js e MySQL. Formado em Técnico em Desenvolvimento de Sistemas pelo Instituto Social Nossa Senhora de Fátima, estou em busca de oportunidades para aplicar minhas habilidades no desenvolvimento de aplicações completas, tanto no front-end quanto no back-end. Meu objetivo é continuar aprendendo e crescer profissionalmente, contribuindo para projetos desafiadores e de impacto.
+            </p>
           </div>
+        </section>
 
-          <div className='conetudo'>
-          {cards.map((card, index) => (
-            <Card key={index} imagem={card.imagem} nome={card.nome} descricao={card.descricao} />
-          ))}
+        <section id="habilidades">
+          <div className="habilidades">
+            <div className="titulo">
+              <h1>Minhas habilidades</h1>
+            </div>
+
+            <div className="conetudo">
+              {cards.map((card, index) => (
+                <Card key={index} imagem={card.imagem} nome={card.nome} descricao={card.descricao} />
+              ))}
+            </div>
           </div>
+        </section>
 
-        </div>
-
-          </section>
-
-          <section id='projetos'>
-
-
-        <div className='Projetos'>
-
-          <div className='titulo'>
-            <h1>Meus Projetos</h1>
-            <div className='linha'></div>
-          </div>
-          <div className='meus-trabalhos'>
-            
-            <a href="http://4.172.207.208:3056/">     
-          <Projetu 
-                imagem={Site} 
-                titulo="Doctor's Health" 
-                descricao="TCC" 
-                data="28 de outubro de 2024" 
-                
+        <section id="projetos">
+          <div className="Projetos">
+            <div className="titulo">
+              <h1>Meus Projetos</h1>
+              <div className="linha"></div>
+            </div>
+            <div className="meus-trabalhos">
+              <a href="http://4.172.207.208:3056/">
+                <Projetu
+                  imagem={Site}
+                  titulo="Doctor's Health"
+                  descricao="TCC"
+                  data="28 de outubro de 2024"
                 />
-                </a>
+              </a>
+            </div>
           </div>
-        </div>
-                </section>
+        </section>
       </div>
     </div>
   );
 }
-
 
 export default App;
