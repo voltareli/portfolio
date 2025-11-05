1a011f2d0ca5605ac68bddfd1f58839c3b4266fb
voltareli <nivoltareli@gmail.com>
2025-11-04 22:53:30 -0300

    oi

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,6 +10,7 @@ import Node from '../assets/node.png';
 import Projetu from '../components/card-projeto/projeto';
 import Site from '../assets/site.png';
 import automacao from '../assets/automação.png';
+import Eosinnov2 from '../assets/eosinnov2.png';
 import Cv from '../assets/cv.pdf';
 import { motion } from 'framer-motion';
 import VanillaTilt from 'vanilla-tilt';
